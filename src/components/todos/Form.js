4f4f4d6425939fd18ac05fd066ae93f652767d25
Carson Meiling <carmc93@gmail.com>
2020-03-16 20:56:04 -0600

    first commit

---
+++ src/components/todos/Form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+
+
+  render() {
+    return (
+      <form>
+
+        
+      </form>
+    )
+  }
+}
+
+export default From;
---
+++ src/components/todos/List.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const List = ({name, todos}) => (
+  <div>
+    <h1>{name}</h1>
+    <ul>
+      {
+        todos.map( todo => <li key={todo.id}>{todo.title}</li>)
+      }
+    </ul>
+  </div>
+
+)
+
+export default List;
