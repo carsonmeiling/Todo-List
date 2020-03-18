c54d215fdfcd615621a87d96bd150d3d70c3ac35
Carson Meiling <carmc93@gmail.com>
2020-03-17 19:07:53 -0600

    added filters and form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
 import React, { Component} from 'react';
 import List from './components/todos/List';
+import Form from './components/todos/Form';
+import Footer from './components/todos/Footer';
 
 class App extends Component{
 state = { todos: [
   {id: 1, title: 'Learn Rails', complete: true},
   {id: 2, title: 'Learn React', complete: true},
   {id: 3, title: 'Learn Hooks', complete: false},
-]}
+],
+filter: 'All'
+}
+
+setFilter = (incomingFilter) => {
+this.setState({ filter: incomingFilter })
+}
+
+visibleTodos = () => {
+  const { todos, filter } = this.state
+  switch(filter){
+    case 'Active':
+      return todos.filter( t => !t.complete )
+    case 'Complete':
+      return todos.filter(t => t.complete )
+    default:
+      return todos;
+  }
+}
+
+getUniqId = () => {
+  //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+ }
+
+ addTodo = (todo) => {
+   const { todos } = this.state
+   const newTodo = { id: this.getUniqId(), title: todo.title, complete: false }
+   this.setState({ todos: [newTodo, ...todos]})
+ }
 
 
 
   render(){
-    const {todos} = this.state
+    const {todos, filter} = this.state
     return(
       <div>
-        <List name='Coding todo list' todos={todos} />
+        <Form addTodo={this.addTodo} />
+        <List name='Coding todo list' todos={this.visibleTodos()} />
+        <Footer filter={filter} setFilter={this.setFilter} />
       </div>
     )
   }
--- src/components/todos/Form.js
+++ src/components/todos/Form.js
@@ -2,15 +2,39 @@ import React, { Component } from 'react';
 
 class Form extends Component {
 
+  state = { title: '' }
+
+  //e = event, updates the state to what the user type for each input
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value})
+  } 
+
+  handleSubmit = (e) => {
+    //stop the page from reloading and losing data, and not in the url bar
+    e.preventDefault()
+    // console.log(this.state.title)
+    // add todo in our array
+    this.props.addTodo(this.state)
+    this.setState({title: ''})
+  }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
+        <input
 
-        
+          required
+          placeholder='add todo'
+
+          name='title'
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+        <button>Submit</button>
       </form>
     )
   }
 }
 
-export default From;
+export default Form;
